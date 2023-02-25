547e5766424ed6608680beb09c59394a68255a57
Levi <levi@leviticusrhoden.com>
2023-02-25 12:53:38 -0800

    '9

--- wedding/wedding.php
+++ wedding/wedding.php
@@ -4,7 +4,7 @@
 Rhoden Wedding
 </title>
 </head>
-
+<body>
 <h1>
 Emma & Levi Rhoden
 </h1>
@@ -25,52 +25,37 @@ Emma & Levi Rhoden
 </dl>
 <br><br>
 
+<h2 id="rsvp">
+RSVP
+</h2>
+<p>
+
+</p>
+<br><br>
+
+<h2 id="directions">
+Directions
+</h2>
+<p>
+You can drive there! Not much else. Reach out to us if you need/can offer to drive for a carpool.
+</p>
+
 <h2 id="itinerary">
 Itinerary
 </h2>
 <p>
 HEY Y"ALL WE GETTING MARRIED, ON A DAY OR SOMETHING IDK.
 </p>
-><br><br>
+<br><br>
 
-<h2>
-Examples of Embedding
+<h2 id="photos">
+Photos
 </h2>
 <p>
-<a href="hostingpi.php">A link example, can be put inside paragraphs of text.</a>
+Engagement photos, and maybe eventually our wedding photos!
 </p>
-<address>
-<a href="hostingpi.php">A link example, but it's centered!</a>
-</address>
 <br><br>
 
-<img src="./crafting/photos/camera_roll_mechanism.jpg" alt="A few long arms, a rachet, and a small arm that stops a cam from turning." style="display: block; margin-left: auto; margin-right: auto;">
-<figcaption>
-    An example of an embeded image that I resize to either 350px or 500px wide, to save space, bandwidth, and so I don't have to mess with scaling html-side.
-</figcaption><br><br>
-
-<img src="./photos/pikachu_construction.gif" width="300px" class="center">
-
-<figcaption>An important gif to let the people know a page is unfinished.</figcaption>
-
-<img src="https://web.archive.org/web/20220323015114im_/https://media.giphy.com/media/B7eXvaDYdHv8NDTM0v/giphy.gif" width="560px" class="center">
-<figcaption>
-Figure 1, A .gif of a fluid simulation I programmed. If you look closely you can see it did not work!
-</figcaption><br><br>
-<iframe width="560" height="315" src="https://web.archive.org/web/20220323015114if_/https://www.youtube.com/embed/CUZq7yudbSU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<figcaption>
-Figure 2, One of my best YouTube videos! Embedding it on your own is a pain, but YouTube has a handy html generator function, so it takes care of it all. Both img and iframe are centered in the styles.css file.
-</figcaption>
-<br><br>
-
-<address>
-<a href="/index.php">leviticusrhoden.com Homepage</a>
-</address>
-<address>
-<a href="styleguide.php">Back to Style Guide (replace with parent page)</a>
-</address>
-<address><a href="rss.xml"> RSS Feed</a></address>
-<address><a href="mailto: levi@leviticusrhoden.com">Send Me an E-Mail!</a></address>
 </body>
 
 </html>
