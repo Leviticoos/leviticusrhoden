e6c14f14c0533488af6abe46327441571288c569
Levi <levi@leviticusrhoden.com>
2023-02-25 20:29:27 -0800

    font size 2

--- wedding/wedding.php
+++ wedding/wedding.php
@@ -12,13 +12,13 @@ Rhoden Wedding
 
 <dl>
 <dt style="font-size: 20pt;"><address><a href="#rsvp">RSVP</a></address></dt>
-<dt><address><a href="#directions">Directions</a></address></dt>
-<dt><address><a href="#itinerary">Itinerary</a></address></dt>
-<dt><address><a href="#photos">Photos</a></address></dt>
-<dt><address><a href="#hallmark">The Plot of our Hallmark Movie</a></address></dt>
-<dt><address><a href="#party">Wedding Party Members</a></address></dt>
-<dt><address><a href="#hotels">Hotels</a></address></dt>
-<dt><address><a href="#registry">Registry</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#directions">Directions</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#itinerary">Itinerary</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#photos">Photos</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#hallmark">The Plot of our Hallmark Movie</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#party">Wedding Party Members</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#hotels">Hotels</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#registry">Registry</a></address></dt>
 </dl>
 <br><br>
 
