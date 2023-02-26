d2de0f1acc887c0ee4508c99cf08c5b0ad7887ca
Levi <levi@leviticusrhoden.com>
2023-02-25 20:42:27 -0800

    Moved wedding.php out of folder. ALso now /wedding sohuld open it

--- wedding.php
+++ wedding.php
@@ -1,3 +1,90 @@
+<html>
 <head>
-    <meta http-equiv="refresh" content="0; URL=https://leviticusrhoden.com/wedding/wedding.php" />
+<title>
+Rhoden Wedding
+</title>
 </head>
+<body>
+
+<link href="/css/weddingstyles.css" rel="stylesheet" type="text/css">
+
+<img src="/photos/pikachu_construction.gif" width="300px" class="center">
+
+<dl>
+<dt style="font-size: 20pt;"><address><a href="#rsvp">RSVP</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#directions">Directions</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#itinerary">Itinerary</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#photos">Photos</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#hallmark">The Plot of our Hallmark Movie</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#party">Wedding Party Members</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#hotels">Hotels</a></address></dt>
+<dt style="font-size: 20pt;"><address><a href="#registry">Registry</a></address></dt>
+</dl>
+<br><br>
+
+<h1 id="rsvp">
+RSVP
+</h1>
+<p>
+
+</p>
+<br><br>
+
+<h1 id="directions">
+Directions
+</h1>
+<p>
+You can drive there! Not much else. Reach out to us if you need/can offer to drive for a carpool.
+</p>
+
+<h1 id="itinerary">
+Itinerary
+</h1>
+<p>
+HEY Y"ALL WE GETTING MARRIED, ON A DAY OR SOMETHING IDK.
+</p>
+<br><br>
+
+<h1 id="photos">
+Photos
+</h1>
+<p>
+Engagement photos, and maybe eventually our wedding photos!
+</p>
+<br><br>
+
+<h1 id="hallmark">
+The Plot of our Hallmark Movie
+</h1>
+<p>
+
+</p>
+<br><br>
+
+<h1 id="party">
+Wedding Party Members
+</h1>
+<p>
+
+</p>
+<br><br>
+
+<h1 id="hotels">
+Hotels
+</h1>
+<p>
+
+</p>
+<br><br>
+
+<h1 id="registry">
+Registry
+</h1>
+<p>
+
+</p>
+<br><br>
+
+</body>
+
+</html>
