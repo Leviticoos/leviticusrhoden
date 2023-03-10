09646fcbf08787c0eebdbf7fa26500b999b1ea07
Leviticoos <leviticus.rhoden@gmail.com>
2023-03-09 16:37:43 -0800

    Started Linkages work

---
+++ linkages/linkages.php
@@ -0,0 +1,40 @@
+<html>
+<!page has been copy edited oct 2022>
+<head>
+<link href="../css/styles.css" rel="stylesheet" type="text/css">
+<title>
+Leviticus' Website
+</title>
+</head>
+
+<body>
+
+<h1>
+Linkages
+</h1>
+
+<address>
+<a href="../index.php">leviticusrhoden.com Homepage</a>
+</address>
+
+<p>
+    Linkages! People still care about them, I'm sure of it! Right? Well, I'm doing a bit of stuff involving 'em, so I figured I'd make a page. Future projects include a linkage calculator, which has a funky angle averager thing I'll also detail here. And hopefully I'll have a decent collection of 4-Bar function generators, with things like log(x), log(log(x)), etc.
+</p><br><br>
+
+<h2><a href="nonPlanar3D.php">COMING SOON: Angle Averager</a></h2>
+<p>
+    This device takes to rotational inputs, and returns a weighted average of the input angles. This will hopefully act as the core of a calculator of sorts.
+</p><br><br>
+
+<h2><a href="nonPlanar3D.php">COMING SOON: Log(x) 4-Bar Function Generator</a></h2>
+<p>
+    I stole a 4-Bar mechanism!!!!
+</p><br><br>
+
+<address>
+<a href="../index.php">leviticusrhoden.com Homepage</a>
+</address>
+
+</body>
+
+</html>
