4da7904da1bef34b2ba992959dbf8c937f71c8ea
Leviticoos <105832125+Leviticoos@users.noreply.github.com>
2023-03-20 09:19:38 -0700

    Printer notes started

---
+++ crafting/enderNotes.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>
+Leviticus' Website
+</title>
+</head>
+
+<h1>NOTES ON UPGRADING ENDER 3 FIRMWARE</h1>
+<br>
+
+<link href="../css/styles.css" rel="stylesheet" type="text/css">
+
+<address>
+<a href="/index.php">leviticusrhoden.com Homepage</a>
+</address>
+<address>
+<a href="triode.php">Back to Triode Page</a>
+</address>
+
+<h2>Sources</h2>
+<dt> -<a href="https://www.youtube.com/watch?v=sKIlgikb3VM&t=1007s"> Great Youtube Video</a></dt>
+<dt> -Simplifier's Excellent Work</dt>
+<dd> -<a href='https://simplifier.neocities.org/tubeconst.html'>Glasswork for Vacuum Tubes</a></dd>
+<dd> -<a href='https://simplifier.neocities.org/triode.html'>His First Crack at Tubes</a></dd>
+<dd> -<a href='https://simplifier.neocities.org/triode2.html'>2nd "</a></dd>
+<dd> -<a href='https://simplifier.neocities.org/triode3.html'>3rd "</a></dd>
+<dd> -<a href='https://simplifier.neocities.org/triode4.html'>4th "</a></dd>
+<br><br>
+
+<h2>What settup do I have?</h2>
+<p> I belive I have a <b>4.2.x</b> board thanks to <a href="https://www.youtube.com/watch?v=P42dO3ygWOE">this mans.</a> I have a "c" on my SD slot, meaning I have <b>
+
+<address>
+<a href="/index.php">leviticusrhoden.com Homepage</a>
+</address>
+<address>
+<a href="triode.php">Back to Triode Page</a>
+</address>
+<address><a href="rss.xml"> RSS Feed</a></address>
+<address><a href="https://web.archive.org/web/20220323015114/mailto: levi@leviticusrhoden.com">Send Me an E-Mail!</a></address>
+</body>
+
+</html>
