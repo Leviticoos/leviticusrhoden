9244d28c8dfa24440ef1a36cc2538ce5e0d084c5
Leviticoos <105832125+Leviticoos@users.noreply.github.com>
2023-09-04 19:30:48 -0700

    Notes@!

---
+++ Notes/c++GameNotes.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+<title>
+Leviticus' Website
+</title>
+</head>
+
+<h1>NOTES ON C++ RENDERING OF TRANSFORMED TILEMAP</h1>
+<br>
+
+<link href="../css/styles.css" rel="stylesheet" type="text/css">
+
+<address>
+<a href="/index.php">leviticusrhoden.com Homepage</a>
+</address>
+<address>
+<a href="game.php">NO LINK</a>
+</address>
+
+<h2>Sources</h2>
+<dt> -<a href="https://www.youtube.com/watch?v=yEiZalvDOj4"> Suraj Sharma's Video on how to set up SFML files such that Visual Studios can use them.</a></dt>
+<dd> -<a href='https://glusoft.com/sfml-tutorials/import-tiled-map/'> How to load Tiled (caps mean it's the format) data, and use SFML to display it</a></dd>
+<dd> -<a href="https://github.com/Tencent/rapidjson"> RapidJSON reader github</a></dd>
+<dd> -<a href="https://gcc.gnu.org/"> g++ compiler</a></dd>
+<dd> -<a href="https://learnubuntu.com/install-gcc/"> g++ install instructions</a></dd>
+<br><br>
+
+<h2>The Goal</h2>
+<p>
+    I want to define a transform that I can use to turn an infinitly large grid/world map into a finite space. Some sort of transform that'll give us some sort of looking into the distance. 
+</p><br><br>
+
+<h2>The Transform</h2>
+<p>
+    i'll get to this TODO
+</p><br><br>
+
+<h2>The Rendering</h2>
+<p>
+    I will be using SFML, a sort of library? thing for openGL. I don't get all the nuances of c++ yet, so I don't want to hear it. The flow will be:
+</p>
+<dt>-Extract map and sprite data from Tiled file</dt>
+<dt>-Work new vertecies from transform</dt>
+<dt>-Display!</dt>
+<dt>-Update as character moves</dt>
+<p>
+        Seems achivable!
+</p>
+
+<address>
+<a href="/index.php">leviticusrhoden.com Homepage</a>
+</address>
+<address>
+<a href="triode.php">Back to Triode Page</a>
+</address>
+<address><a href="rss.xml"> RSS Feed</a></address>
+<address><a href="https://web.archive.org/web/20220323015114/mailto: levi@leviticusrhoden.com">Send Me an E-Mail!</a></address>
+</body>
+
+</html>
