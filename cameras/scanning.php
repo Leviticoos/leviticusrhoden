ba65980970267a7c1ad448eecf413e842161c3e1
Leviticoos <105832125+Leviticoos@users.noreply.github.com>
2024-04-15 15:15:27 -0700

    Create scanning.php

---
+++ cameras/scanning.php
@@ -0,0 +1,87 @@
+<html>
+<head>
+<title>
+Leviticus' Website
+</title>
+</head>
+
+<h1>
+Page Title
+</h1>
+
+<link href="../css/styles.css" rel="stylesheet" type="text/css">
+
+<address>
+<a href="../index.php">leviticusrhoden.com Homepage</a>
+</address>
+<address>
+<a href="/cameras.php">Back to Cameras</a>
+</address>
+
+<img src="/photos/pikachu_construction.gif" width="300px" class="center">
+
+<h2>
+First subheading, often Introduction
+</h2>
+<p>
+First Paragraph. This is meant to be a copy and paste-able file so I can use the same code to make a page and embed stuff in it. Essentially, it's where I test out any code I want to use.
+</p><p>
+Second paragraph, notice the double line break after.
+</p><br><br>
+
+<h2>
+Another Subheading
+</h2>
+<p>
+Paragraph 1.
+</p><p>
+Paragraph 2. This one I made longer to showcase how it drops down a line, it's quite interesting. Also appreciate <a href="hostingpi.php">this link</a> as an example of link html. Remember to put a <b>../</b> in the address for each step back in the directories you need to go from the current page before you can go down a branch to the page you want.
+</p><br><br>
+
+<h2>
+Examples of Embedding
+</h2>
+<p>
+<a href="hostingpi.php">A link example, can be put inside paragraphs of text.</a>
+</p>
+<address>
+<a href="hostingpi.php">A link example, but it's centered!</a>
+</address>
+<br><br>
+
+<img src="./crafting/photos/camera_roll_mechanism.jpg" alt="A few long arms, a rachet, and a small arm that stops a cam from turning." style="display: block; margin-left: auto; margin-right: auto;">
+<figcaption>
+    An example of an embeded image that I resize to either 350px or 500px wide, to save space, bandwidth, and so I don't have to mess with scaling html-side.
+</figcaption><br><br>
+
+<img src="./photos/pikachu_construction.gif" width="300px" class="center">
+
+<figcaption>An important gif to let the people know a page is unfinished.</figcaption>
+
+<img src="https://web.archive.org/web/20220323015114im_/https://media.giphy.com/media/B7eXvaDYdHv8NDTM0v/giphy.gif" width="560px" class="center">
+<figcaption>
+Figure 1, A .gif of a fluid simulation I programmed. If you look closely you can see it did not work!
+</figcaption><br><br>
+<iframe width="560" height="315" src="https://web.archive.org/web/20220323015114if_/https://www.youtube.com/embed/CUZq7yudbSU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<figcaption>
+Figure 2, One of my best YouTube videos! Embedding it on your own is a pain, but YouTube has a handy html generator function, so it takes care of it all. Both img and iframe are centered in the styles.css file.
+</figcaption>
+<br><br>
+
+<h2>
+    Annotated Bibliography
+</h2>
+<dt><a href="https://francoismalan.com/how-to-batch-separate-crop-multiple-scanned-photos/">Source 1, francoismalan.com, accessed 01/01/2000</a></dt>
+    <dd>Something to look at to use gimp to sus out individual photos from a busy scan.</a></dd>
+
+<address>
+<a href="/index.php">leviticusrhoden.com Homepage</a>
+</address>
+<address>
+<a href="styleguide.php">Back to Style Guide (replace with parent page)</a>
+</address>
+<address><a href="rss.xml"> RSS Feed</a></address>
+<address><a href="mailto: levi@leviticusrhoden.com">Send Me an E-Mail!</a></address>
+</body>
+
+</html>
