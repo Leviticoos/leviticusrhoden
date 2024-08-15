e75ecc97749186ab5d599122fff0af42357c328c
Leviticoos <105832125+Leviticoos@users.noreply.github.com>
2024-08-15 10:49:40 -0700

    Update hillyard.php

--- trains/hillyard.php
+++ trains/hillyard.php
@@ -21,10 +21,24 @@ Spokane Hillyard Switching Layout
 <img src="./photos/pikachu_construction.gif" width="300px" class="center">
 
 <h2>
+The Hillyard
+</h2>
+<p>
+The Spokane Hillyard was a huge facility owned and run by the Great Northern Railroad. It had many shops to facilitate maintenece of the locomotives, rolling stock, and other infrastructure of the railroad. It also had a couple large switching yards, and a couple industries to serve depending on the era. 
+</p>
+<img src="photos/hillyardMap.jpg" alt="A spagetti of train tracks, TODO" style="display: block; margin-left: auto; margin-right: auto;">
+<figcaption>
+    An ariel view of the Hillyard, taken TODO
+</figcaption><br><br> 
+<p>
+Paragraph 2. This one I made longer to showcase how it drops down a line, it's quite interesting. Also appreciate <a href="hostingpi.php">this link</a> as an example of link html. Remember to put a <b>../</b> in the address for each step back in the directories you need to go from the current page before you can go down a branch to the page you want.
+</p><br><br>
+    
+<h2>
 The Layout
 </h2>
 <p>
-For this layout, I wanted to make a switching layout about a foot and a half deep, and however long it needs to be. I'm lucky enough to have a (albeit grainy) map of the hillyard. I'll be looking for a more detailed map this summer. I plan to take a trip to spokane, it's about a 5 hour drive from my home Seattle, and see if their local archives have any cool history. That'll make it's way here.
+For this layout, I wanted to make a switching layout about a foot and a half deep, and however long it needs to be. I'm lucky enough to have a (albeit grainy) map of the hillyard. I'll be looking for a more detailed map this summer. I plan to take a trip to Spokane, it's about a 5 hour drive from my home in Seattle, and see if their local archives have any cool history. That'll make it's way here.
 </p><br>
 <img src="photos/hillyardMap.jpg" alt="A spagetti of train tracks, TODO" style="display: block; margin-left: auto; margin-right: auto;">
 <figcaption>
@@ -35,13 +49,43 @@ Second paragraph, notice the double line break after.
 </p><br><br>
 
 <h2>
-The Hillyard
+Locomotive Production
 </h2>
 <p>
-Paragraph 1.
-</p><p>
-Paragraph 2. This one I made longer to showcase how it drops down a line, it's quite interesting. Also appreciate <a href="hostingpi.php">this link</a> as an example of link html. Remember to put a <b>../</b> in the address for each step back in the directories you need to go from the current page before you can go down a branch to the page you want.
+One of my hopes for this layout is to get to run locomotive repair, overhaul, and even assembly operations (ops). That includes reciving boxcars with parts from other manufacturers (usually things like lamps, gauges, etc.), reciving raw materials (lumbar, steel ingots, sheet metal, maybe rivets), the nebulus ~building things~, and final assembly. This also would include the transfer of parts between and within the buildings in the Hillyard. If this sounds like a lofty goal, it is! And it was quite the adventure to get there.
+</p>
+<p>
+    This journey started with the book Superpower by TODO TODO [d], which proved to be a great starting point for how a locomotive works, well, works. Written from the perspective of a young man just starting his employ at the LIMA locomotive works, we follow him helping out at each stage of the creation of a new locomotive. This gives a great narative idea of not only the process of building a steam locomotive, but also how the people in this shop lived, interacted with each other, and how they went about their work. This is invaluable information when the buildings and tracks are placed, and it's time to fill the layout with people going about their lives.
+    The book breaks the production process into a few steps:
+    <dt>Design; engineers and drafters in an office of sorts.</dt>
+    <dt>Carving; carpenters making wooden 1:1 models of the designed cast parts</dt>
+    <dt>Casting; the wooden parts are stamped into casting sand to make the molds, then melted metal is poured to make parts like the chasiss, wheels, etc.</dt>
+    <dt></dt>
+</p>    
+<br>
+<img src="photos/hillyardMap.jpg" alt="A spagetti of train tracks, TODO" style="display: block; margin-left: auto; margin-right: auto;">
+<figcaption>
+    The Spokane Hillyard Track Plan
+</figcaption><br><br>
+<p>
+Second paragraph, notice the double line break after.
 </p><br><br>
+
+    
+<h2>
+Switching
+</h2>
+<p>
+it's a switch layout, so.
+</p><br>
+<img src="photos/hillyardMap.jpg" alt="A spagetti of train tracks, TODO" style="display: block; margin-left: auto; margin-right: auto;">
+<figcaption>
+    The Spokane Hillyard Track Plan
+</figcaption><br><br>
+<p>
+Second paragraph, notice the double line break after.
+</p><br><br>
+
 
 <h2>
 Examples of Embedding
@@ -77,8 +121,12 @@ Figure 2, One of my best YouTube videos! Embedding it on your own is a pain, but
     Annotated Bibliography
 </h2>
 <dt><a href="https://www.trainboard.com/highball/index.php?media/hillyard-map-steam-era-with-full-roundhouse-and-icing-facilities.1448/">Source a, Trainboard.com</a></dt>
-    <dd>This source was a great resource for how i could do x, y, and z.</a></dd>
+    <dd>This is a collection of Hillyard Photos (TODO change link to home of album), including a couple good shots of the Hillyard itself and an (sadly blurry) map of the hillyard rails.</a></dd>
 <dt><a href="https://www.northwestmuseum.org/collections/research-archives/">Source b, Contact these blokes about No. L85-143.38, Arial view of Hillyard ca 1942</dt>
+<dt><a href="https://streamlinermemories.info/?p=8481">Source c, Streamliner Memories.info</a></dt>
+    <dd>A cememorative booklet published by Great Northern. Goes over semi-in depth history of the spot.</dd>
+<dt><a href="https://streamlinermemories.info/?p=8481">Source d, Superpower by TODO TODO</a></dt>
+    <dd>THIS BOOK KICKS BUTT</dd>
 
 <address>
 <a href="/index.php">leviticusrhoden.com Homepage</a>
